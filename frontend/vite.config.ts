fce31712c139fd0400685b68e26bdfd753974d05
Tanishq Singh <hi@tanishqsingh.com>
2025-02-01 14:55:47 +0530

    frontend + backend basic

---
+++ frontend/vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+
+export default defineConfig({
+	plugins: [react()],
+	resolve: {
+		alias: {
+			"@components": resolve(__dirname, "/src/components"),
+			"@lib": resolve(__dirname, "/src/lib"),
+		},
+	},
+});
